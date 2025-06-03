91119b335df6faf6a8a2aa5f4674b97ace7bef70
IngaSam <Kayumovainga2@mail.ru>
2025-06-03 14:53:58 +0300

    add create user

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,7 +4,10 @@ import com.example.demo.repository.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,8 +18,16 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public List<User> helloWorld(){
+    public List<User> findAll(){
         return userRepository.findAll(); //возвращает все записи из БД
 
+    }
+    public User create(User user){
+        Optional <User> optionalUser=userRepository.findByEmail(user.getEmail());
+        if(optionalUser.isPresent()){
+            throw new IllegalArgumentException("Пользователь с таким имейлом уже существует");
+        }
+        user.setAge(Period.between(user.getBirth(), LocalDate.now()).getYears()); //подсчет возраста
+        return userRepository.save(user);
     }
 }
